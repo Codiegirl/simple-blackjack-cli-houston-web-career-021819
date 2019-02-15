279f95df446d01d97fac206336a07fdc581265c5
Ro Flo <iroflood@msn.com>
2019-02-15 23:52:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,6 +55,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+   hand=initial_round
+   until hand>21 do
+     hand=hit?(hand)
+     display_card_total(hand)
+   end
+   end_game(hand)
+ end		 # code runner here
 end
     
